185e3831668f6e6bc1e223da6bf540af752a1c8a
gamze <gamzekandara@gmail.com>
2024-04-28 23:17:54 -0400

    Adds control_flow.py

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,49 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    return "Access granted" if (username == "admin" or username == "ADMIN") and password == "12345" else "Access denied"
+
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 < temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:  # Check if the number is divisible by both 3 and 5
+        return "FizzBuzz"
+    elif num % 3 == 0:  # Check if the number is divisible by 3
+        return "Fizz"
+    elif num % 5 == 0:  # Check if the number is divisible by 5
+        return "Buzz"
+    else:
+        return num
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        if operation == "+":
+            return num1 + num2
+        elif operation == "-":
+            return num1 - num2
+        elif operation == "*":
+            return num1 * num2
+        elif operation == "/":
+            return num1 / num2
+        else:
+            print("Invalid operation!")
+            return None 
+    except ValueError:
+        return "Invalid value"
+
+    # try/except for exception/error handling
+
